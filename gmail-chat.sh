3677abfa8894a00821afe8b9e2ac493306a7c577
Kimbsy <dave.kimber@simitive.com>
2015-03-11 15:36:47 +0000

    traffic lights for IMs and emails

--- gmail-chat.sh
+++ gmail-chat.sh
@@ -1,10 +1,15 @@
-# figure out if I have an unread gmail IM by checking the title of chrome
+# figure out if I have an unread gmail IM or email by checking the title of chrome
 while sleep 1; do
   if wmctrl -l | grep -q 'says...'; then
-    echo good
-    echo 'y' > /dev/ttyUSB1
+    echo IM
+    echo 'rbh' > /dev/ttyUSB0
   else
-    echo bad
-    echo 'n' > /dev/ttyUSB1
+    if wmctrl -l | grep -q 'Inbox ('; then
+      echo email
+      echo 'ybf' > /dev/ttyUSB0
+    else
+      echo all systems nominal
+      echo 'gfh' > /dev/ttyUSB0
+    fi
   fi
 done
--- sketches/gmailChat/gmailChat.ino
+++ sketches/gmailChat/gmailChat.ino
@@ -1,35 +1,83 @@
 #define RED 2
+#define YEL 3
+#define GRE 4
 
 #define RED_HI() digitalWrite(RED, HIGH);
 #define RED_LO() digitalWrite(RED, LOW);
+#define YEL_HI() digitalWrite(YEL, HIGH);
+#define YEL_LO() digitalWrite(YEL, LOW);
+#define GRE_HI() digitalWrite(GRE, HIGH);
+#define GRE_LO() digitalWrite(GRE, LOW);
 
-boolean active = false;
+boolean red = false;
+boolean yellow = false;
+boolean green = false;
+
 
 void setup() {
   pinMode(RED, OUTPUT);
+  pinMode(YEL, OUTPUT);
+  pinMode(GRE, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
+  
   if (Serial.available() > 0) {
     int input = Serial.read();
+    int delayTime = 0;
     
     switch(input) {
-      case 'y':
-        active = true;
+      // allow user input delay
+      case 'd':
+        delayTime = Serial.parseInt();
+        delay(delayTime);
         break;
-      case 'n':
-        active = false;
+      // red led
+      case 'r':
+        red = true;
+        break;
+      case 'f':
+        red = false;
+        break;
+      //yellow led
+      case 'y':
+        yellow = true;
+        break;
+      case 'h':
+        yellow = false;
+        break;
+      //green led
+      case 'g':
+        green = true;
+        break;
+      case 'b':
+        green = false;
         break;
       default:
         break;
     }
   }
   
-  if (active) {
+  if (red) {
     RED_HI();
   }
   else {
     RED_LO();
   }
+  
+  if (yellow) {
+    YEL_HI();
+  }
+  else {
+    YEL_LO();
+  }
+  
+  if (green) {
+    GRE_HI();
+  }
+  else {
+    GRE_LO();
+  }
 }
+
