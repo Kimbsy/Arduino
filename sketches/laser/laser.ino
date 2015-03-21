ccc3ef68bc2b36996a965b7406910c5c8212fe1f
Kimbsy <lordkimber@gmail.com>
2015-03-21 13:31:40 +0000

    2 axis servo aiming for laser

--- sketches/laser/laser.ino
+++ sketches/laser/laser.ino
@@ -2,17 +2,34 @@
  * Basic laser control.
  * 220 Ohm resistor from pin 2 to 'S'.
  * Ground pin to '-'.
+ *
+ * Two servos for 2 axis movement. 
  */
+ 
+#include <Servo.h>
 
 int laserPin = 2;
 
+Servo rotate;
+Servo elevate;
+
 void setup() {
   pinMode(laserPin, OUTPUT);
+  
+  rotate.attach(3);
+  elevate.attach(4);
 }
 
 void loop() {
   digitalWrite(laserPin, HIGH);
+  
+  rotate.write(180);
+  elevate.write(180);
+  
   delay(1000);
-//  digitalWrite(laserPin, LOW);
+  
+  rotate.write(0);
+  elevate.write(0);
+  
   delay(1000);
 }
