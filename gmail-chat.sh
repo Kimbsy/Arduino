d71787c797f15afb483c9988767119facdbf61b7
Kimbsy <dave.kimber@simitive.com>
2015-04-30 09:07:04 +0100

    colorized terminal output

--- gmail-chat.sh
+++ gmail-chat.sh
@@ -1,14 +1,21 @@
 # figure out if I have an unread gmail IM or email by checking the title of chrome
+
+# define colors
+RED='\033[0;31m'
+GRE='\033[0;32m'
+YEL='\033[0;33m'
+NC='\033[0m'
+
 while sleep 1; do
   if wmctrl -l | grep -q 'says...'; then
-    echo IM
+    printf "${RED}IM${NC}\n"
     echo 'rbh' > /dev/ttyUSB0
   else
     if wmctrl -l | grep -q 'Inbox ('; then
-      echo email
+      printf "${YEL}email${NC}\n"
       echo 'ybf' > /dev/ttyUSB0
     else
-      echo all systems nominal
+      printf "${GRE}all systems nominal${NC}\n"
       echo 'gfh' > /dev/ttyUSB0
     fi
   fi
