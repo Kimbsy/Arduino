c288dac6af47ed773b6263ceadee5a45b243ad39
Kimbsy <dave.kimber@simitive.com>
2015-05-12 16:21:46 +0100

    slack private convo IM

--- gmail-chat.sh
+++ gmail-chat.sh
@@ -7,10 +7,10 @@ YEL='\033[0;33m'
 NC='\033[0m'
 
 while sleep 1; do
-  if wmctrl -l | grep -q "[*].*Simitive"; then
+  if wmctrl -l | grep -q "[*].*Simitive" || wmctrl -l | grep -q "[!].*"; then
     printf "${RED}Slack-IM${NC}\n"
     if [ "$*" != "noled" ]; then
-      echo 'rygd100fhbd100rygd100fhbd100rygd100fhbd100rygd100fhbd100rygd100fhbd100' > /dev/ttyUSB0
+      echo 'rbh' > /dev/ttyUSB0
     fi
   else
     if wmctrl -l | grep -q 'says...'; then
