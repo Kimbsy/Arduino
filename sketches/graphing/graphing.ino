bbd5fef1d01b1712ac2e2e163c72fcb27726674b
Kimbsy <lordkimber@gmail.com>
2015-06-21 13:32:11 +0100

    made it better

--- sketches/graphing/graphing.ino
+++ sketches/graphing/graphing.ino
@@ -1,17 +1,10 @@
-double count = 0;
+/**
+ * Arduino sketch for 6ch receiver testing suite.
+ *
+ * Detects signal frequency and sends data over serial bus.
+ */
 
-long min1 = 9999999;
-long min2 = 9999999;
-long min3 = 9999999;
-long min4 = 9999999;
-long min5 = 9999999;
-long min6 = 9999999;
-long max1 = 0;
-long max2 = 0;
-long max3 = 0;
-long max4 = 0;
-long max5 = 0;
-long max6 = 0;
+long sigVals[] = {0, 0, 0, 0, 0, 0};
 
 void setup()
 {
@@ -26,39 +19,16 @@ void setup()
 
 void loop()
 {
-  long sig1 = pulseIn(2, INPUT, 50000);
-  long sig2 = pulseIn(3, INPUT, 50000);
-  long sig3 = pulseIn(4, INPUT, 50000);
-  long sig4 = pulseIn(5, INPUT, 50000);
-  long sig5 = pulseIn(6, INPUT, 50000);
-  long sig6 = pulseIn(7, INPUT, 50000);
-
-  if (sig1 > max1) max1 = sig1;
-  if (sig2 > max2) max2 = sig2;
-  if (sig3 > max3) max3 = sig3;
-  if (sig4 > max4) max4 = sig4;
-  if (sig5 > max5) max5 = sig5;
-  if (sig6 > max6) max6 = sig6;
+  sigVals[0] = pulseIn(2, INPUT, 50000);
+  sigVals[1] = pulseIn(3, INPUT, 50000);
+  sigVals[2] = pulseIn(4, INPUT, 50000);
+  sigVals[3] = pulseIn(5, INPUT, 50000);
+  sigVals[4] = pulseIn(6, INPUT, 50000);
+  sigVals[5] = pulseIn(7, INPUT, 50000);
   
-  if (sig1 < min1) min1 = sig1;
-  if (sig2 < min2) min2 = sig2;
-  if (sig3 < min3) min3 = sig3;
-  if (sig4 < min4) min4 = sig4;
-  if (sig5 < min5) min5 = sig5;
-  if (sig6 < min6) min6 = sig6;
-
-  // value, fromlow, fromhigh, tlow, tohigh
-  Serial.print(1023 - map(sig1, min1, max1, 0, 1023));
-  Serial.print(",");
-  Serial.print(1023 - map(sig2, min2, max2, 0, 1023));
-  Serial.print(",");
-  Serial.print(1023 - map(sig3, min3, max3, 0, 1023));
-  Serial.print(",");
-  Serial.print(1023 - map(sig4, min4, max4, 0, 1023));
-  Serial.print(",");
-  Serial.print(1023 - map(sig5, min5, max5, 0, 1023));
-  Serial.print(",");
-  Serial.print(1023 - map(sig6, min6, max6, 0, 1023));
+  for (int i = 0; i < 6; i++) {
+    Serial.print(sigVals[i]);
+    Serial.print(",");
+  }
   Serial.print("\n");
-  count++;
 }
