74e5bdff79050c4398f53d5856b01064f230633a
Kimbsy <lordkimber@gmail.com>
2015-07-04 10:39:17 +0100

    #tmp36 sketch to alculate temperature

--- gmail-chat.sh
+++ gmail-chat.sh
@@ -7,28 +7,21 @@ YEL='\033[0;33m'
 NC='\033[0m'
 
 while sleep 1; do
-  if wmctrl -l | grep -q "[*].*Simitive" || wmctrl -l | grep -q "[!].*Simiitve"; then
-    printf "${RED}Slack-IM${NC}\n"
+  if wmctrl -l | grep -q 'says...'; then
+    printf "${RED}IM${NC}\n"
     if [ "$*" != "noled" ]; then
       echo 'rbh' > /dev/ttyUSB0
     fi
   else
-    if wmctrl -l | grep -q 'says...'; then
-      printf "${RED}IM${NC}\n"
+    if wmctrl -l | grep -q 'Inbox ('; then
+      printf "${YEL}email${NC}\n"
       if [ "$*" != "noled" ]; then
-        echo 'rbh' > /dev/ttyUSB0
+        echo 'ybf' > /dev/ttyUSB0
       fi
     else
-      if wmctrl -l | grep -q 'Inbox ('; then
-        printf "${YEL}email${NC}\n"
-        if [ "$*" != "noled" ]; then
-          echo 'ybf' > /dev/ttyUSB0
-        fi
-      else
-        printf "${GRE}all systems nominal${NC}\n"
-        if [ "$*" != "noled" ]; then
-          echo 'gfh' > /dev/ttyUSB0
-        fi
+      printf "${GRE}all systems nominal${NC}\n"
+      if [ "$*" != "noled" ]; then
+        echo 'gfh' > /dev/ttyUSB0
       fi
     fi
   fi
---
+++ sketches/tmp36/tmp36.ino
@@ -0,0 +1,32 @@
+/**
+ * Measures the temperature using a TMP36 high acuracy temperature sensor.
+ *
+ * Facing flat side of sensor:
+ *   left => 5V
+ *   middle => analogue pin 0
+ *   right => ground
+ */
+
+#define A_IN 0
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // read voltage from TMP36
+  int reading = analogRead(A_IN);
+
+  // convert to voltage (use 3.3 for 3.3V power)
+  float voltage = reading * 5;
+  voltage /= 1024;
+
+  // 10mv per degree, with 500mv as offset for negative temps
+  float tempC = (voltage - 0.5) * 100;
+
+  Serial.println(tempC);
+
+  delay(1000);  
+}
