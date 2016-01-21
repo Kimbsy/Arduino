7fb237684ad12213563a2870085e6caf7754d035
Kimbsy <lordkimber@gmail.com>
2016-01-21 21:30:40 +0000

    changing up goal sketch

--- sketches/gooooooooooal/gooooooooooal.ino
+++ sketches/gooooooooooal/gooooooooooal.ino
@@ -1,33 +1,45 @@
-// sketch to read values from a Photodiode HI DAVE
+#define OUT_1 2
+#define OUT_2 3
+#define OUT_3 4
 
-int Photo_diode_Value = 0;
+#define OUT_1_HI() digitalWrite(OUT_1, HIGH);
+#define OUT_1_LO() digitalWrite(OUT_1, LOW);
+#define OUT_2_HI() digitalWrite(OUT_2, HIGH);
+#define OUT_2_LO() digitalWrite(OUT_2, LOW);
+#define OUT_3_HI() digitalWrite(OUT_3, HIGH);
+#define OUT_3_LO() digitalWrite(OUT_3, LOW);
 
-int score = 0;
+void triggerGoal();
 
-const int Photo_diode_Pin = 0;  // arduino pin that sensor happens to be hooked up to
-const int IR_LED_pin = 11;
-
-void setup() 
+void setup()
 {
-    Serial.begin(9600);    // open serial port to laptop to print values
-    analogWrite(IR_LED_pin, 255);
+  pinMode(OUT_1, OUTPUT);  
+  pinMode(OUT_2, OUTPUT);  
+  pinMode(OUT_3, OUTPUT);  
 }
 
-void loop() 
+void loop()
 {
-    Photo_diode_Value = analogRead(Photo_diode_Pin);            // Read analog Pin 
-//    Serial.print("   Photo diode reads = ");      // print text string 
-//    Serial.println(Photo_diode_Value, DEC);       // print value of variable
-    
-    Serial.println(score);
-    if (Photo_diode_Value == 0) {
-      goal();
-    }
-
-    delay(30);        // delay so the program does not run too fast
+  triggerGoal();
 }
 
-void goal() {
-  score++;
-  delay(500);
+void triggerGoal()
+{
+  // first
+  OUT_1_HI();
+  delay(100);
+  OUT_1_LO();
+  delay(1000);
+
+  // second
+  OUT_2_HI();
+  delay(100);
+  OUT_2_LO();
+  delay(1000);
+
+  // third
+  OUT_3_HI();
+  delay(100);
+  OUT_3_LO();
+  delay(1000);
 }
